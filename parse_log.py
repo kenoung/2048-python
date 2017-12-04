a37e1e691cb430e9302cd3e8ff5b3dbe6d1e30bc
Ken <ken@datatron.io>
2017-12-03 23:38:31 -0800

    add analyze notebook

--- parse_log.py
+++ parse_log.py
@@ -4,9 +4,8 @@ regex_moves = re.compile('avg no of moves: ([\d.]+)')
 regex_mean_q = re.compile('mean q value: ([\d.]+)')
 
 if __name__ == '__main__':
-    with open('data', 'r') as f:
+    with open('result/2048-ddqn-sparse-32-0.95-0.0001-lp-de-True.log', 'r') as f:
         for line in f:
-            if 'Perf' in line:
-                avg_moves = regex_moves.search(line).group(1)
-                mean_q = regex_mean_q.search(line).group(1)
-                print('{},{}'.format(avg_moves, mean_q))
+            if 'loss' in line:
+                json_data = line.split('INFO ')[0]
+                parsed_data = json.loads(json_data)
