757f536781d8602fc4215a6938f8a36e3c626c57
Ken <ken@datatron.io>
2017-12-10 23:07:50 -0800

    greedy policy

--- test_model.py
+++ test_model.py
@@ -7,7 +7,14 @@ import sys
 from DDQNAgent import DDQNAgent
 from grid import Grid
 
-def eval_agent(agent, env, NO_OF_RUNS):
+
+def get_greedy_action(env):
+    moves = [(move, env.standard_score_wo_lose_penalty(move)) for move in env.get_available_moves()]
+    best_move, score = max(moves, key=lambda x: x[1])
+    return best_move
+
+
+def eval_agent(agent, env, NO_OF_RUNS, greedy=False):
     max_tiles = {}
     scores = 0
     move_counts = 0
@@ -17,7 +24,11 @@ def eval_agent(agent, env, NO_OF_RUNS):
         curr_score = 0
         while not env.is_game_over():
             state = np.reshape(env.get_curr_state(), [1, env.state_size])
-            move = agent.act_policy(state, env.get_available_moves())
+            agent.epsilon = 1
+            if greedy:
+                move = get_greedy_action(env)
+            else:
+                move = agent.act_policy(state, env.get_available_moves())
             curr_score += env.standard_score_wo_lose_penalty(move)
             move_count += 1
             env.play(move)
@@ -34,10 +45,11 @@ def eval_agent(agent, env, NO_OF_RUNS):
     print("avg score:", scores/NO_OF_RUNS)
     print("avg moves:", move_counts/NO_OF_RUNS)
 
+
 if __name__ == '__main__':
-    WEIGHTS_FILE = 'result/2048-ddqn-sparse-32-0.95-0.0001-lp-de-True.h5'
+    WEIGHTS_FILE = 'result/2048-ddqn-sparse-32-0.95-0.0001-sswolp-de-True.h5'
     EXPERIMENT_NAME = WEIGHTS_FILE.split('/')[1]
-    NO_OF_RUNS = 100
+    NO_OF_RUNS = 1000
 
     env = Grid(4)
     agent = DDQNAgent(env.state_size, env.action_size)
